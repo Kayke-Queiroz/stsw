6e90fdf7a4369f54c69dff086132320f448ca1b1
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-08-14 20:14:17 +0000

    tutorials serenity

---
+++ lectures/examples/03-todomvc-cucumber-serenitybdd/todomvc/src/main/java/br/edu/idp/stsw/bdd/Main.java
@@ -0,0 +1,8 @@
+package br.edu.idp.stsw.bdd;
+
+//mvn exec:java -Dexec.mainClass=br.edu.idp.stsw.bdd.Main
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
