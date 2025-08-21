8cdab827bf33ae4ec0b97de8b19c5812417f49e6
mateus3007 <mateus3007araujo@gmail.com>
2025-08-21 14:18:56 -0300

    Exercicio cumcumber mais maven

---
+++ assignments/02-triangle-bdd/submissions/mateus-almeida/src/main/java/triangulo/Triangulo.java
@@ -0,0 +1,22 @@
+package triangulo;
+
+public class Triangulo  {
+    public static String classificarTriangulo(int a, int b, int c){
+
+        if (a <= 0 || b <= 0 || c <= 0 || a > 200 || b > 200 || c > 200) {
+            return "Lados invalidos";
+        }
+
+        if (a + b < c || a + c < b || b + c < a) {
+            return "Não é um triângulo";
+        }
+
+        if (a == b && b == c) {
+            return "Equilátero";
+        } else if (a == b || a == c || b == c) {
+            return "Isósceles";
+        } else {
+            return "Escaleno";
+        }
+    }
+}
