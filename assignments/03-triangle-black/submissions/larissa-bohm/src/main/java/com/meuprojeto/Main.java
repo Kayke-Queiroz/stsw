3d7e901c5a4e5845d4ef3fe8a73941e9fa063f53
lalabohm <lalala.bohm@gmail.com>
2025-08-29 10:57:43 -0300

    ajustando o código e os testes

--- assignments/03-triangle-black/submissions/larissa-bohm/src/main/java/com/meuprojeto/Main.java
+++ assignments/03-triangle-black/submissions/larissa-bohm/src/main/java/com/meuprojeto/Main.java
@@ -5,17 +5,56 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            Entrada entrada = new Entrada(scanner);
+            // Entrada entrada = new Entrada(scanner);
 
-            int a = entrada.lerValor("a");
-            int b = entrada.lerValor("b");
-            int c = entrada.lerValor("c");
+            int valor;
+            int a = 0;
+            int b = 0;
+            int c = 0;
 
-            Triangulo t = new Triangulo(a, b, c);
+            while (true) {
+                System.out.println("Digite o lado do triângulo: ");
+                if (scanner.hasNextInt()) {
+                    valor = scanner.nextInt();
+                    if (valor >= 1 && valor <= 200) {
+                        a = valor;
+                    } else {
+                        System.out.println("Erro: O valor não está entre 1 e 200.");
+                    }
+                } else {
+                    System.out.println("Erro: o valor não é um inteiro válido. Por favor, digite outro número:");
+                    scanner.next();
+                }
 
-            System.out.println(t.tipo());
+                System.out.println("Digite o lado do triângulo: ");
+                if (scanner.hasNextInt()) {
+                    valor = scanner.nextInt();
+                    if (valor >= 1 && valor <= 200) {
+                        b = valor;
+                    } else {
+                        System.out.println("Erro: O valor não está entre 1 e 200.");
+                    }
+                } else {
+                    System.out.println("Erro: o valor não é um inteiro válido. Por favor, digite outro número:");
+                    scanner.next();
+                }
+
+                System.out.println("Digite o lado do triângulo: ");
+                if (scanner.hasNextInt()) {
+                    valor = scanner.nextInt();
+                    if (valor >= 1 && valor <= 200) {
+                        c = valor;
+                    } else {
+                        System.out.println("Erro: O valor não está entre 1 e 200.");
+                    }
+                } else {
+                    System.out.println("Erro: o valor não é um inteiro válido. Por favor, digite outro número:");
+                    scanner.next();
+                }
+                Triangulo t = new Triangulo(a, b, c);
+                System.out.println(t.tipo());
+            }
         }
-
     }
 
 }
