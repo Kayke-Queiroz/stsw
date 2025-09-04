2c20ef9bd008f62bf409ff31bb1fc1c72d7631da
Lucas Barreto <93289129+LucasB712@users.noreply.github.com>
2025-09-03 21:32:46 -0300

    Update RunCucumberTest.java

--- assignments/03-triangle-black/submissions/lucas-barreto/src/test/java/br/edu/idp/es/stsw/hellocucumber/runners/RunCucumberTest.java
+++ assignments/03-triangle-black/submissions/lucas-barreto/src/test/java/br/edu/idp/es/stsw/hellocucumber/runners/RunCucumberTest.java
@@ -1,11 +1,11 @@
-package br.edu.idp.es.stsw.hellocucumber.runners;
+package br.edu.idp.es.stsw.triangle.runners;
 
 import io.cucumber.core.cli.Main;
 
 public class RunCucumberTest {
         public static void main(String[] args) {
         Main.main(new String[]{
-            "-g", "br.edu.idp.es.stsw.hellocucumber.steps",
+            "-g", "br.edu.idp.es.stsw.triangle.steps",
             "src/test/resources/features"
         });
     }
