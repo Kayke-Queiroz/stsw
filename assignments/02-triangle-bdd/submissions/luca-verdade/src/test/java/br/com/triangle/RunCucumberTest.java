5ef532dbab207853d3a9d0235597973bc2856789
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2026-02-23 01:11:25 +0000

    limpando submissões para próximo semestre

--- assignments/02-triangle-bdd/submissions/luca-verdade/src/test/java/br/com/triangle/RunCucumberTest.java
+++
@@ -1,7 +0,0 @@
-package br.com.triangle;
-
-import io.cucumber.junit.platform.engine.Cucumber;
-
-@Cucumber
-public class RunCucumberTest {
-}
--- assignments/02-triangle-bdd/submissions/mateus-batista/src/main/java/br/edu/idp/es/stsw/hellocucumber/IsItFriday.java
+++
@@ -1,7 +0,0 @@
-package br.edu.idp.es.stsw.hellocucumber;
-
-public class IsItFriday {
-    public static String isItFriday(String today) {
-        return "Friday".equals(today) ? "TGIF" : "Nope";
-    }
-}
